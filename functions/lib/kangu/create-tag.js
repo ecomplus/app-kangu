44f93d539b3dc75bf6f6219a0c4d35c25e798a45
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-07-29 11:38:19 -0300

    chore: remove object

--- functions/lib/kangu/create-tag.js
+++ functions/lib/kangu/create-tag.js
@@ -99,9 +99,7 @@ module.exports = async (order, token, storeId, appData, appSdk) => {
         produto.altura = cmDimensions.height || 0,
         produto.largura = cmDimensions.width || 0,
         produto.comprimento = cmDimensions.length || 0
-        data.produtos.push({
-          produto
-        })
+        data.produtos.push(produto)
       })
       .catch(err => {
         console.log(err.message)

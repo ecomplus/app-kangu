7a9c714c91229d705ba92ccb7755ec042b64b5a3
Leonardo Matos <leomp120894@gmail.com>
2024-09-02 02:01:51 -0300

    chore: no need to filter orders by custom field on tracking (better by tracking code)

--- functions/lib/kangu/check-orders-tracking.js
+++ functions/lib/kangu/check-orders-tracking.js
@@ -36,7 +36,6 @@ const fetchUndeliveredOrders = async ({ appSdk, storeId }) => {
           d2.setHours(d2.getHours() - 2)
           const endpoint = '/orders.json' +
             '?fields=_id,number,fulfillment_status,shipping_lines' +
-            '&shipping_lines.custom_fields.field=kangu_reference' +
             '&shipping_lines.tracking_codes.tag=kangu' +
             '&financial_status.current=paid' +
             '&fulfillment_status.current!=delivered' +
